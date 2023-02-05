2434e9a56b153b89edc003486c0e0a06f6c1a640
binilvvtl <binilvvtl@gmail.com>
2023-02-05 12:00:36 +0530

    Initial commit

---
+++ VSApp/VSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VSApp
+//
+//  Created by Binil-V on 05/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
