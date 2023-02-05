b0e35608b22377b4e69189bd2a11e49cbbe46258
binilvvtl <binilvvtl@gmail.com>
2023-02-05 23:55:47 +0530

    added pod file

---
+++ VSApp/Pods/Target Support Files/Pods-VSApp-VSAppUITests/Pods-VSApp-VSAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VSApp_VSAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_VSApp_VSAppUITests
+@end
---
+++ VSApp/Pods/Target Support Files/Pods-VSApp/Pods-VSApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VSApp : NSObject
+@end
+@implementation PodsDummy_Pods_VSApp
+@end
